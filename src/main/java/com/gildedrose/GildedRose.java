4b807789f8201fa8e18ff1714911b0125d46abeb
StephenHales <stephen.james.hales@gmail.com>
2025-01-17 17:02:39 -0700

    Refactor Brie logic and remove bits of code that aren't used now

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -12,10 +12,12 @@ class GildedRose {
     public void updateQuality() {
 
         for (int i = 0; i < items.length; i++) {
+            // Sulfuras (Not hand of Ragnaros)
             if (items[i].name.equals("Sulfuras, Hand of Ragnaros")){ //TODO rename this, this is not the name
                 continue;
             }
 
+            // Aged Brie
             if (items[i].name.equals("Aged Brie")){
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
@@ -24,9 +26,8 @@ class GildedRose {
                 continue;
             }
 
-            //Quality logic
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            // Quality logic
+            if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
                     items[i].quality = items[i].quality - 1;
                 }
@@ -54,20 +55,14 @@ class GildedRose {
             items[i].sellIn = items[i].sellIn - 1;
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
+                if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (items[i].quality > 0) {
+                        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                            items[i].quality = items[i].quality - 1;
                         }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+                    items[i].quality = 0;
                 }
             }
         }
